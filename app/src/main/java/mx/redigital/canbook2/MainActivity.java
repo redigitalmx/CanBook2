2fdbc3420b26e998967cbf242cbc42374ef9121f
redigitalmx <redigital@gmail.com>
2016-12-14 13:41:59 -0600

    Update

--- app/src/main/java/mx/redigital/canbook2/MainActivity.java
+++ app/src/main/java/mx/redigital/canbook2/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -27,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar miActionBar =(Toolbar)findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionBar);
+
 
        listaMascotas = (RecyclerView)findViewById(R.id.rvMascotas);
 
